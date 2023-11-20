e0d77c5bd767401d0e4353b06b7e742fdd38952a
Zhuofan Zhang <zhangzhuofan2019@163.com>
2023-11-20 21:42:15 +0000

    fix mockito test
    Co-authored-by: Ruijing Zhao <zhaor1@tcd.ie>

--- test/MockNetworkChecker.dart
+++
@@ -1,4 +0,0 @@
-import 'package:dynamic_way_finding/RealNetworkChecker.dart';
-import 'package:mockito/mockito.dart';
-
-class MockNetworkChecker extends Mock implements RealNetworkChecker {}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,32 +1,51 @@
+import 'package:dynamic_way_finding/ErrorPage.dart';
+import 'package:dynamic_way_finding/RealNetworkChecker.dart';
 import 'package:dynamic_way_finding/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'MockNetworkChecker.dart';
+import 'NetworkdCheckerTest.mocks.dart';
 
+// Create a Mock class
+// class MockNetworkChecker extends Mock implements RealNetworkChecker {}
+
+// Annotation which generates the mockNetworkChecker.mocks.dart library
+@GenerateMocks([RealNetworkChecker])
 void main() {
-  late MockNetworkChecker mockNetworkChecker;
+  group('MyApp Tests', () {
+    late MockRealNetworkChecker mockNetworkChecker;
 
-  setUp(() {
-    mockNetworkChecker = MockNetworkChecker();
-  });
+    setUp(() {
+      // Initialize the mock
+      mockNetworkChecker = MockRealNetworkChecker();
+    });
 
-  testWidgets('App should run and render MyApp widget',
-      (WidgetTester tester) async {
-    when(mockNetworkChecker.hasNetworkConnection()).thenReturn(true);
+    testWidgets('should render GoogleMap when network is available',
+        (WidgetTester tester) async {
+      // Arrange
+      when(mockNetworkChecker.hasNetworkConnection())
+          .thenReturn(true); // Ensuring a boolean value is returned
 
-    runApp(MyApp(networkChecker: mockNetworkChecker));
+      // Act
+      await tester.pumpWidget(MyApp(networkChecker: mockNetworkChecker));
 
-    expect(find.byType(MyApp), findsOneWidget);
-  });
+      expect(find.byType(MyApp), findsOneWidget);
+      expect(find.byType(GoogleMap), findsOneWidget);
+    });
 
-  testWidgets('Renders Google map', (WidgetTester tester) async {
-    when(mockNetworkChecker.hasNetworkConnection()).thenReturn(true);
+    testWidgets('should show ErrorPage when network is unavailable',
+        (WidgetTester tester) async {
+      // Arrange
+      when(mockNetworkChecker.hasNetworkConnection())
+          .thenReturn(false); // Ensuring a boolean value is returned
 
-    await tester.pumpWidget(MyApp(networkChecker: mockNetworkChecker));
+      // Act
+      await tester.pumpWidget(MyApp(networkChecker: mockNetworkChecker));
 
-    expect(find.byType(GoogleMap), findsOneWidget);
+      // Assert
+      expect(find.byType(ErrorPage), findsOneWidget);
+    });
   });
 }
